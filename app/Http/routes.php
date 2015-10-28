82bec38748acc10d5e3c2d0ed68b0e352b607512
Daiyrbek Artelov <dartelov@intelliants.com>
2015-10-28 11:41:13 +0600

    routes updated, testing sourcetree

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,10 +17,19 @@ Route::get('/', function () {
 
 Route::group([
     'prefix' => 'admin',
-//    'middleware' => 'auth',
+    'middleware' => 'Admin',
     'namespace' => 'Admin',
 ], function () {
     Route::get('/', function () {
         return view('admin.dashboard');
     });
 });
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
