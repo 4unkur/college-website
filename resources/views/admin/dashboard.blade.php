d9087fad5ec3ef61b40f9081e8c50da581991455
Daiyrbek Artelov <dartelov@intelliants.com>
2015-12-09 01:34:22 +0600

    Working on #2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,5 @@
 <!doctype html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,42 +14,20 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="AdminLTE/dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-          page. However, you can choose any other skin. Make sure you
-          apply the skin class to the body tag so the changes take effect.
-    -->
+
     <link rel="stylesheet" href="AdminLTE/dist/css/skins/skin-green.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->
+
 <body class="hold-transition skin-green sidebar-mini">
 <div class="wrapper">
+
 @include('admin.header')
+
 @include('admin.sidebar')
 
     <!-- Content Wrapper. Contains page content -->
@@ -139,12 +113,11 @@ desired effect
             </div><!-- /.tab-pane -->
         </div>
     </aside><!-- /.control-sidebar -->
-    <!-- Add the sidebar's background. This div must be placed
-         immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
+
 </div><!-- ./wrapper -->
+
 @include('admin.footer')
-<!-- REQUIRED JS SCRIPTS -->
+
 
 <!-- jQuery 2.1.4 -->
 <script src="AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
@@ -153,9 +126,5 @@ desired effect
 <!-- AdminLTE App -->
 <script src="AdminLTE/dist/js/app.min.js"></script>
 
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. Slimscroll is required when using the
-     fixed layout. -->
 </body>
 </html>
---
+++ resources/views/partials/slider.blade.php
@@ -0,0 +1,26 @@
+<section class="slider">
+    <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="item"> <img data-src="images/slider/slider.jpg" alt="First slide" src="images/slider/slider.jpg">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <p class="little"><span>Quisque et tellus lorem</span></p>
+                        <h1>Nullam at nulla consequat purus feugiat vehicula</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat.</p>
+                        <p><a class="btn btn-default btn-lg" href="#" role="button">learn more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item active"> <img data-src="images/slider/slider1.jpg" alt="Second slide" src="images/slider/slider1.jpg">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <p class="little"><span>Lorem ipsum dolor site.</span></p>
+                        <h1>Nam pellentesque condimentum erat ac consequat</h1>
+                        <p>Consectetur pharetra a, scelerisque ac mi. In lacus nulla, auctor sed felis.</p>
+                        <p><a class="btn btn-default btn-lg" href="#" role="button">get started</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon carousel-control-left"></span></a> <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon carousel-control-right"></span></a> </div>
+</section>
