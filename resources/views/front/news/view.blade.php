467aeb4207baaa928aa245f83464422cc2a739a9
4unkur <4unkur@mail.ru>
2016-05-05 23:18:54 +0600

    Closes #19

--- resources/views/front/news/view.blade.php
+++ resources/views/front/news/view.blade.php
@@ -14,6 +14,18 @@
             <article class="text">
                 {!! $news->text !!}
             </article>
+            <div id="disqus_thread"></div>
+            <script>
+                (function() {
+                    var d = document, s = d.createElement('script');
+
+                    s.src = '//diploma-daiyrbek.disqus.com/embed.js';
+
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
         </section>
         @include('front.news.block-random')
     </div>
